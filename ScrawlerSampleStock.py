c4a47f4470845f612ffbd6d63bb64a659a6626e3
Evelyn <LinYongYangag@gmail.com>
2026-05-26 23:22:46 +1000

    ID: Evelyn Description: top 20 potential stocks retrieved successfully Root Cause: none Solution: none

--- ScrawlerSampleStock.py
+++ ScrawlerSampleStock.py
@@ -1,4 +1,8 @@
 # pip install lxml
+from io import StringIO
+import traceback
+
+
 def check_import(module_name, alias=None):
     try:
         module = __import__(module_name)
@@ -23,10 +27,13 @@ check_import("yfinance", "yf")
 check_import("warnings")
 check_import("traceback")
 check_import("sys")
+check_import ("lxml") # pd.read_html 需要的解析器
+
+from io import StringIO # 不直接把整個 io 套件 import 進來，只 import 其中一個 function
 
 
-# warnings.filterwarnings('ignore') # 關掉幾乎所有 Python warning
-# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # 關掉 urllib3 的 HTTPS warning
+warnings.filterwarnings('ignore') # 關掉幾乎所有 Python warning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # 關掉 urllib3 的 HTTPS warning
 
 print("="*70) # 印出 70 個 =
 print("   任務：掃描台股全市場，尋找 AI 7 大動能極限飆股")
@@ -34,70 +41,87 @@ print("   核心：捨棄 RSI/MACD/成交量，純粹依賴價格破壞力與波
 print("   視野：跨市場百分比排名 (Percentile Rank)")
 print("="*70)
 
+def save_stock_list(stock_dict):
+    try:
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop") # 目前使用者的 home directory，就是 C:\Users\{username}，然後再加上 Desktop 就是桌面路徑
+        csv_filename = os.path.join(desktop_path, "TW_Stock_List_.csv")
+        df_stock_list = pd.DataFrame.from_dict(stock_dict, orient='index')
+        df_stock_list.index.name = 'ID'
+        df_stock_list.reset_index(inplace=True)
+        df_stock_list.to_csv(csv_filename, index=False, encoding='utf-8-sig')
+        
+        print(f"台股清單已儲存至桌面：{csv_filename}")
+    except:
+        print("無法儲存 CSV 檔案至桌面，請確認權限。")
+
 def get_tw_stock_list():
-    print("[1/3] 抓取全台股清單 (上市+上櫃)...")
+    print("[1/3] 抓取全台股清單 (上市+上櫃+興櫃)...")
     stock_dict = {}
     try:
         headers = {'User-Agent': 'Mozilla/5.0'}
         for Page in [2, 4, 5]: # in (2, 5) 才是 2、3、4
             url = f"https://isin.twse.com.tw/isin/C_public.jsp?strMode={Page}"
             res = requests.get(url, headers=headers, verify=False, timeout=15)
-            df = pd.read_html(res.text)[0].iloc[1:] # df = dataframe
+            df = pd.read_html(StringIO(res.text), flavor="lxml")[0].iloc[2:] # df = dataframe     
             for index, row in df.iterrows(): # 逐 row 讀取 DataFrame，每次回傳 (index, row)，但我們不需要 index，所以用 _ 代替
                 try:
                     code_name = str(row[0]).split()
+                    print(f"{code_name}...", end='\r')
                     if len(code_name) == 2:
                         code, name = code_name
                         cat = str(row[3]) # category
                         if len(code) == 4:
                             if cat in ['上市', '上櫃', '興櫃']:
-                                suffix = ".TW" if Page == 2 else ".TWO"
+                                suffix = ".TW" if cat == '上市' else ".TWO"
                                 stock_dict[f"{code}{suffix}"] = {"name": name, "ind": cat} # "": key, : value 
-                except: continue
+                except Exception as e:
+                    print(f"row error: {type(e).__name__}")
     except Exception as e: 
-        print(f"抓取清單失敗: {e}") # f: 直接在字串裡放變數
+        print(f"抓取清單失敗: {type(e).__name__}")
+
+    save_stock_list(stock_dict)
     return stock_dict
 
-def history_volatility(close_price):
+def history_volatility(close_prices):
     # day1_close = 100, day2_close = 110, pct_change = (110 - 100) = 10%
     # daily return: 每日報酬率
-    daily_ret = close_price.pct_change()
+    daily_ret = close_prices.pct_change()
     # std: 標準差，看資料有多分散
     hist_vol = daily_ret.rolling(20).std().iloc[-1] * np.sqrt(252) * 100
     return hist_vol
 
-def moving_average(close_price, window):
+def moving_average(close_prices, window):
     # rolling: 每次看連續 5 天收盤價，[100 102 101 105 107]
     # mean(): 算平均
     # iloc[-1]: 最後一 row
-    return close_price.rolling(window).mean().iloc[-1]
+    return close_prices.rolling(window).mean().iloc[-1]
 
-def bollinger_band_width(close_price): # 布林通道，價格大部分時間應該待在通道裡
-    ma20 = close_price.rolling(20).mean() 
-    std20 = close_price.rolling(20).std() # 最近20天價格的標準差
+def bollinger_band_width(close_prices): # 布林通道，價格大部分時間應該待在通道裡
+    ma20 = close_prices.rolling(20).mean() 
+    std20 = close_prices.rolling(20).std() # 最近20天價格的標準差
     bb_upper = ma20 + 2 * std20
     bb_lower = ma20 - 2 * std20
-    return (bb_upper - bb_lower) / ma20 * 100 # 4 * std20 / ma20 * 100
+    return ((bb_upper - bb_lower) / ma20 * 100).iloc[-1] # 4 * std20 / ma20 * 100
 
-def price_to_bollinger_upper (close_price):
-    ma20 = close_price.rolling(20).mean() 
-    std20 = close_price.rolling(20).std()
-    current_close = close_price.iloc[-1] # 取今日(就是最後一個)收盤價
+def price_to_bollinger_upper (close_prices):
+    ma20 = close_prices.rolling(20).mean() 
+    std20 = close_prices.rolling(20).std()
+    current_close = close_prices.iloc[-1] # 取今日(就是最後一個)收盤價
     bb_upper = ma20 + 2 * std20
 
     return (current_close / bb_upper.iloc[-1] - 1) * 100
 
-def record_add(records_db, Name, Id, close_price):
+def record_add(records_db, Name, Id, close_prices):
 
-    ma5 = moving_average(close_price, 5)
-    ma20 = moving_average(close_price, 20)
-    ma60 = moving_average(close_price, 60)
-    current_close = close_price.iloc[-1]
+    ma5 = moving_average(close_prices, 5)
+    ma20 = moving_average(close_prices, 20)
+    ma60 = moving_average(close_prices, 60)
+    current_close = close_prices.iloc[-1]
 
     # 特徵 1: 歷史波動率
-    hist_vol = history_volatility(close_price)
+    hist_vol = history_volatility(close_prices)
     # 特徵 2: 布林通道寬度
-    bb_width = bollinger_band_width(close_price)
+    bb_width = bollinger_band_width(close_prices)
     # 特徵 3: 股價距離 MA60 幾 %
     p_to_ma60 = (current_close / ma60 - 1) * 100
     # 特徵 4: 短均線 vs 長均線 強度
@@ -105,9 +129,9 @@ def record_add(records_db, Name, Id, close_price):
     # 特徵 5: 股價距離 MA20 幾 %
     p_to_ma20 = (current_close / ma20 - 1) * 100
     # 特徵 6: 股價距離布林通道上緣幾 %
-    p_to_bbupper = price_to_bollinger_upper(close_price)
+    p_to_bbupper = price_to_bollinger_upper(close_prices)
     # 特徵 7: 10 日價格變動率
-    roc_10 = (current_close - close_price.iloc[-11]) / close_price.iloc[-11] * 100
+    roc_10 = (current_close - close_prices.iloc[-11]) / close_prices.iloc[-11] * 100
     
     # 檢查 hist_vol 或 roc_10 是不是 NaN
     if np.isnan(hist_vol) or np.isnan(roc_10): return 
@@ -168,14 +192,14 @@ def top_20_dump(top20):
 def top_20_save(top20):
     try:
         desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
         csv_filename = os.path.join(desktop_path, f"Top20_{timestamp}.csv")
         columns_to_save = ['ID', 'Name', 'Close', 'AI_Score', 'F_Hist_Vol', 'F_BB_Width', 'F_P_to_MA60', 'F_Trend_Strength', 'F_P_to_MA20', 'F_P_to_BBUpper', 'F_ROC_10']
         top20[columns_to_save].to_csv(csv_filename, index=False, encoding='utf-8-sig')
         
         print(f"完整特徵明細已儲存至桌面：Top20_{timestamp}.csv")
     except:
-        print("無法儲存 CSV 檔案至桌面，請確認權限。")
+        traceback.print_exc()
 
 def main():
     stock_dict = get_tw_stock_list()
@@ -187,39 +211,39 @@ def main():
     records = []
     
     # 批次下載歷史資料
-    # for i in range(0, len(all_tickers), batch_size): # e.g., range (0, 5, 2) 會回傳 0, 2, 4
-    #     batch = all_tickers[i:i+batch_size]
-    #     print(f"   下載進度: {min(i+batch_size, len(all_tickers))}/{len(all_tickers)}...", end='\r')
+    for i in range(0, len(all_tickers), batch_size): # e.g., range (0, 5, 2) 會回傳 0, 2, 4
+        batch = all_tickers[i:i+batch_size]
+        print(f"   下載進度: {min(i+batch_size, len(all_tickers))}/{len(all_tickers)}...", end='\r')
         
-    #     try:
-    #         # 抓取 100 天確保 60MA 計算正確
-    #         data = yf.download(batch, period="100d", interval="1d", group_by='ticker', auto_adjust=False, progress=False, threads=True)
+        try:
+            # 抓取 100 天確保 60MA 計算正確
+            data = yf.download(batch, period="100d", interval="1d", group_by='ticker', auto_adjust=False, progress=False, threads=True)
             
-    #         for ticker in batch: # ticker = {code}{suffix}
-    #             try:
-    #                 df = data[ticker] if len(batch) > 1 else data
-    #                 if df.empty or len(df) < 60: continue # 資料不到 60 天
-    #                 df = df.dropna() # 把含有缺失值（NaN）的 row 刪掉
+            for ticker in batch: # ticker = {code}{suffix}
+                try:
+                    df = data[ticker] if len(batch) > 1 else data
+                    if df.empty or len(df) < 60: continue # 資料不到 60 天
+                    df = df.dropna() # 把含有缺失值（NaN）的 row 刪掉
                     
-    #                 close = df['Close'] # 確認收盤價至少有 60 筆
-    #                 if len(close) < 60: continue
-    #                 record_add(records, stock_dict[ticker]['name'], ticker.replace(".TW", "").replace(".TWO", ""), close)
-    #             except: continue
-    #     except: pass
+                    close = df['Close'] # 確認收盤價至少有 60 筆
+                    if len(close) < 60: continue
+                    record_add(records, stock_dict[ticker]['name'], ticker.replace(".TW", "").replace(".TWO", ""), close)
+                except: continue
+        except: pass
 
-    # print("\n資料下載完成！")
-    # print("[3/3] 正在執行 AI 權重運算與全市場 PR 值排名...")
+    print("\n資料下載完成！")
+    print("[3/3] 正在執行 AI 權重運算與全市場 PR 值排名...")
     
-    # df_res = pd.DataFrame(records)
-    # if df_res.empty:
-    #     print("沒有足夠的資料可以運算。")
-    #     return
-    # top20 = top_20_extract(df_res)
+    df_res = pd.DataFrame(records)
+    if df_res.empty:
+        print("沒有足夠的資料可以運算。")
+        return
+    top20 = top_20_extract(df_res)
 
-    # top_20_dump(top20)
-    # top_20_save(top20)
+    top_20_dump(top20)
+    top_20_save(top20)
 
-    # input("👉 程式執行完畢，請按 Enter 鍵關閉視窗...")
+    input("程式執行完畢，請按 Enter 鍵關閉視窗...")
 
 if __name__ == "__main__": # python code 的 entry point
     try:
